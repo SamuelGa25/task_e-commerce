b48fba939c4e881680c78f00a01857d77d71854f
Samuel <samuelgarenas@gmail.com>
2022-07-21 21:30:08 -0400

    trying to fix the issue

--- models/Category.js
+++ models/Category.js
@@ -6,14 +6,14 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
-    id:{
-      type: DataTypes.INTEGER, 
+    id: {
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true, 
-    }, 
-    category_name:{
-      type:DataTypes.STRING,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
       allowNull: false,
     }
   },
